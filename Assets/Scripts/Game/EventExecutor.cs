6b0f4474f00ff8566e085c09e1a113f8e0180e8e
timeless12138 <92915063+timeless12138@users.noreply.github.com>
2023-02-05 02:53:27 +0800

    EventCondition Judgement

--- Assets/Scripts/Game/EventExecutor.cs
+++ Assets/Scripts/Game/EventExecutor.cs
@@ -41,16 +41,70 @@ namespace Roots.Game
                     }
                     break;
                 case EffectCondition.HAS_CHILDREN:
+                    if (character.Children.Count != 0 && condition.Para1 == 1 || character.Children.Count == 0 && condition.Para1 == 0)
+                    {
+                        available= true;
+                        return true;
+                    }
                     break;
                 case EffectCondition.THIS_HAVEDONE:
+                    //TODO
                     break;
                 case EffectCondition.ALL_HAVEDONE:
+                    //TODO
                     break;
                 case EffectCondition.ATTRIMINNEED:
+                    switch (condition.Para1)
+                    {
+                        case (int)AttriType.LINGLI:
+                            if(character.LINGLI >= condition.Para2)
+                            {
+                                available = true;
+                                return true;
+                            }
+                            break;
+                        case (int)AttriType.SHENSHI:
+                            if(character.SHENSHI >= condition.Para2)
+                            {
+                                available = true;
+                                return true;
+                            }
+                            break;
+                        case (int)AttriType.BOWEN:
+                            if (character.BOWEN >= condition.Para2)
+                            {
+                                available = true;
+                                return true;
+                            }
+                            break;
+                        case (int)AttriType.XINGYUN:
+                            if (character.XINGYUN >= condition.Para2)
+                            {
+                                available = true;
+                                return true;
+                            }
+                            break;
+                    }
                     break;
                 case EffectCondition.HAVETAG:
+                    for(int i = 0; i < character.Tags.Count; i++)
+                    {
+                        if (character.Tags[i].TagId == condition.Para2)
+                        {
+                            available = true;
+                            return true;
+                        }
+                    }
                     break;
                 case EffectCondition.HAVERES:
+                    for (int i = 0; i < character.Resources.Count; i++)
+                    {
+                        if (character.Resources[i].ResourceId == condition.Para2)
+                        {
+                            available = true;
+                            return true;
+                        }
+                    }
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
